dbe1389e1cd3d08ffb238f28946e02bcee7e8463
Sharad Bansal <51768458+sharad7900@users.noreply.github.com>
2024-06-26 17:28:52 +0530

    Password_Generator.py

---
+++ Password_Generator.py
@@ -0,0 +1,24 @@
+import string
+import random
+
+if __name__ =="__main__":
+    s1 = string.ascii_lowercase
+    s2 = string.ascii_uppercase
+    s3 = string.digits
+    s4 = string.punctuation
+    
+    s = []
+    
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    
+    random.shuffle(s)
+    
+    length = int(input("Enter the length of password: "))
+    
+    password = ("".join(s[0:length]))
+    
+    print(password)
+    
